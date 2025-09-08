6ae6d78fec18111ebbe3a7d320d96fa6676742c5
Y. P. Lamb <yanpinglam.official@gmail.com>
2025-09-08 12:48:15 +0000

    boardgame.py defined initialize_board() hiddenrules.py added

--- boardgame.py
+++ boardgame.py
@@ -1,1 +1,12 @@
 
+def initialize_board(size = 10):
+    board = []
+    for i in range(size):
+        b=0
+        board.append(list(range(0+size*(size-i-1), size+size*(size-i-1))))
+    return board
+
+board = initialize_board()
+
+for row in board:
+    print(row)
